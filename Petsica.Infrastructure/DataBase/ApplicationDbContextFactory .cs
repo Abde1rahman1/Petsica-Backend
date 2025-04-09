25d309a5aacd1a3b1043d6013575bf7ae614034f
Abdelrahman Sayed <as2863@fayoum.edu.eg>
2025-04-10 00:23:40 +0200

    Handle Connection String V2

--- Petsica.Infrastructure/DataBase/ApplicationDbContextFactory .cs
+++ Petsica.Infrastructure/DataBase/ApplicationDbContextFactory .cs
@@ -15,7 +15,7 @@ public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<Applicati
 		// optionsBuilder.UseSqlServer("Server=(localdb)\\ProjectModels;Database=Petsicaa;Trusted_Connection=True;Encrypt=False");
 
 		//Abdoo
-		optionsBuilder.UseSqlServer("Server=(localdb)\\ProjectModels;Database=Petsica;Trusted_Connection=True;Encrypt=False");
+		optionsBuilder.UseSqlServer("Server=.;Database=Petsicaa;Trusted_Connection=True;Encrypt=False");
 
 		//Public
 		//optionsBuilder.UseSqlServer("Server=db16824.public.databaseasp.net; Database=db16824; User Id=db16824; Password=7f-Q@Aj6n3L?; Encrypt=True; TrustServerCertificate=True; MultipleActiveResultSets=True; ");
